abb0a3f3a07ae7ef8c63f0fdfae9c04bd926ce44
Bingchiling <hhh@hhh.com>
2024-05-18 20:31:04 +0700

    gfdgafdafd

--- QLSV/Program.cs
+++ QLSV/Program.cs
@@ -14,6 +14,7 @@ namespace QLSV
         [STAThread]
         static void Main()
         {
+            //ghagdandhdajhdad 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
